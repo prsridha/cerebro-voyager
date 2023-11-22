6f70f737e11e0f00d164433791875d2761081fb9
prsridha <prsridha@ucsd.edu>
2023-11-21 23:43:14 -0800

    added all run files as executables in sync.sh

--- build/sync.sh
+++ build/sync.sh
@@ -13,8 +13,17 @@ elif [[ "$PLATFORM" == "dev" ]]; then
     clone_status=$?
     if [ $clone_status -eq 0 ]; then
         echo "Successfully cloned the repository."
-        chmod +x /cerebro-core/init.sh
+
+        # add dir as safe in git
         git config --global --add safe.directory /cerebro-core
+
+        # make all run files executable
+        chmod +x /cerebro-core/init.sh
+        chmod +x /cerebro-core/init.sh
+        chmod +x /cerebro-core/cerebro/etl/run_etl.sh
+        chmod +x /cerebro-core/cerebro/mop/run_mop.sh
+        chmod +x /cerebro-core/server/run_server.sh
+        chmod +x /cerebro-core/ui/run_ui.sh
     else
         echo "An error occurred while running git clone."
     fi
