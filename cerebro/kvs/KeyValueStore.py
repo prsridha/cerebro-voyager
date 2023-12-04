8947a6fda0ea50686059d91ca4fe9fe0f814e0ef
prsridha <prsridha@ucsd.edu>
2023-12-04 15:37:59 -0800

    Updated KVS to work on JSON files

--- cerebro/kvs/KeyValueStore.py
+++ cerebro/kvs/KeyValueStore.py
@@ -1,29 +1,38 @@
 import os
 import dill
 import json
-import redis
 import base64
+from pathlib import Path
 from kubernetes import client, config
 
 
 class KeyValueStore:
     def __init__(self, init_tables=False):
-        # get username and namespace
-        namespace = os.environ['NAMESPACE']
-
-        # load Kubernetes config, get Redis host
-        config.load_kube_config()
-        username = os.environ['USERNAME']
-        host = "{}-redis-master.{}.svc.cluster.local".format(username, namespace)
+        base_path = "/key_value_store"
+        self.key_paths = {
+            "kvs_init": os.path.join(base_path, "kvs_init.json"),
+            "seed": os.path.join(base_path, "seed.json"),
+            "error": os.path.join(base_path, "error.json"),
+            "restarts": os.path.join(base_path, "restarts"),
+            "dataset_locators": os.path.join(base_path, "dataset_locators.json"),
+            "etl_task": os.path.join(base_path, "etl_task.json"),
+            "etl_spec": os.path.join(base_path, "etl_spec.json"),
+            "etl_worker_status": os.path.join(base_path, "etl_worker_status"),
+            "etl_worker_progress": os.path.join(base_path, "etl_worker_progress"),
+            "mop_spec": os.path.join(base_path, "mop_spec.json"),
+            "mop_task": os.path.join(base_path, "mop_task.json"),
+            "mop_sample_time": os.path.join(base_path, "mop_sample_time"),
+            "mop_worker_status": os.path.join(base_path, "mop_worker_status"),
+            "mop_model_mapping": os.path.join(base_path, "mop_model_mapping.json"),
+            "mop_model_on_worker": os.path.join(base_path, "mop_model_on_worker"),
+            "mop_parallelism_mapping": os.path.join(base_path, "mop_model_parallelism_mapping.json"),
+            "mop_model_parallelism_on_worker": os.path.join(base_path, "mop_model_parallelism_on_worker.json"),
+        }
 
         # create Kubernetes handle object
+        config.load_kube_config()
         v1 = client.CoreV1Api()
-
-        # create Redis handle object
-        port = 6379
-        passwrd = "cerebro"
-        r = redis.Redis(host, port, decode_responses=True, password=passwrd)
-        self.r = r
+        namespace = os.environ['NAMESPACE']
 
         # get number of workers
         cm = v1.read_namespaced_config_map(name='cerebro-node-hardware-info', namespace=namespace)
@@ -31,158 +40,213 @@ class KeyValueStore:
         self.num_workers = len(node_info)
 
         # initialize KVS with default values
-        init_done = r.get("kvs_init") == "true"
+        if os.path.isfile(self.key_paths["kvs_init"]):
+            with open(self.key_paths["kvs_init"]) as f:
+                init_done = str(json.load(f)) == "true"
+        else:
+            init_done = False
         if init_tables and not init_done:
             self.initialize_tables()
 
     def initialize_tables(self):
-        r = self.r
         num_workers = self.num_workers
 
-        r.set("etl_task", json.dumps(["", ""]))
+        # create required KVS directories
+        Path(self.key_paths["restarts"]).mkdir(exist_ok=True)
+        Path(self.key_paths["etl_worker_status"]).mkdir(exist_ok=True)
+        Path(self.key_paths["etl_worker_progress"]).mkdir(exist_ok=True)
+        Path(self.key_paths["mop_sample_time"]).mkdir(exist_ok=True)
+        Path(self.key_paths["mop_worker_status"]).mkdir(exist_ok=True)
+        Path(self.key_paths["mop_model_on_worker"]).mkdir(exist_ok=True)
 
-        # set empty dataset locators
-        r.set("dataset_locators", "")
+        # set seed
+        self.set_seed(0)
+
+        # set error
+        self.set_error("")
 
         # mark all nodes as healthy
         for w in range(num_workers):
-            r.hset("restarts", str(w), 0)
+            path = os.path.join(self.key_paths["restarts"], str(w) + ".json")
+            with open(path, "w") as f:
+                json.dump(0, f)
 
-        # set etl worker progress to 0 for all workers
+        # set etl task and spec
+        self.etl_set_task("", "")
+        self.etl_set_spec("")
+
+        # set etl worker progress to 0 for all workers, worker status to idle
         for w in range(num_workers):
-            r.hset("etl_worker_progress", str(w), 0)
+            self.etl_set_worker_status(w, "")
+            self.etl_set_worker_progress(w, 0)
 
-        # set etl worker status to idle for all tasks for all workers
-        for w in range(num_workers):
-            r.hset("etl_worker_status", str(w), json.dumps(list()))
-
-        r.set("mop_task", "")
+        # set mop task and spec
+        self.mop_set_spec("")
+        self.mop_set_task("")
 
         # set all mop worker statuses to empty
         for w in range(num_workers):
-            r.hset("mop_worker_status", str(w), "")
-
-        # set all mop model on workers to empty
-        for w in range(num_workers):
-            r.hset("mop_model_on_worker", str(w), json.dumps(list()))
+            self.mop_set_worker_status(w, "")
 
         # mark as initialized
-        r.set("kvs_init", "true")
-
-        # set error value as empty
-        r.set("error", "")
+        path = self.key_paths["kvs_init"]
+        with open(path, "w+") as f:
+            json.dump("true", f)
 
     # dataset locators
     def set_dataset_locators(self, locators):
-        d = json.dumps(locators)
-        res = self.r.set("dataset_locators", d)
-        return res
+        path = self.key_paths["dataset_locators"]
+        with open(path, "w+") as f:
+            json.dump(locators, f)
 
     def get_dataset_locators(self):
-        val = json.loads(self.r.get("dataset_locators"))
+        path = self.key_paths["dataset_locators"]
+        with open(path, "r") as f:
+            val = json.load(f)
         return val
 
     # seed value for randomness
     def set_seed(self, seed):
-        res = self.r.set("seed", str(seed))
-        return res
+        path = self.key_paths["seed"]
+        with open(path, "w+") as f:
+            json.dump(str(seed), f)
 
     def get_seed(self):
-        val = self.r.get("seed")
-        return int(val)
+        path = self.key_paths["seed"]
+        with open(path, "r") as f:
+            val = int(json.load(f))
+        return val
 
     # error on user's code
     def set_error(self, err):
-        self.r.set("etl_task", "")
-        res = self.r.set("error", str(err))
-        return res
+        path = self.key_paths["error"]
+        with open(path, "w+") as f:
+            json.dump(err, f)
 
     def get_error(self):
-        val = self.r.get("error")
+        path = self.key_paths["error"]
+        with open(path, "r") as f:
+            val = json.load(f)
         return val
 
     # health of the cluster
     def set_restarts(self, worker_id):
-        count = int(self.r.hget("restarts", str(worker_id)))
-        res = self.r.hset("restarts", str(worker_id), str(count + 1))
-        return res
+        path = os.path.join(self.key_paths["restarts"], str(worker_id) + ".json")
+        if os.path.isfile(path):
+            with open(path, "r") as f:
+                count = int(json.load(f))
+        else:
+            count = 0
+        count += 1
+        with open(path, "w+") as f:
+            json.dump(str(count), f)
 
     def get_restarts(self, worker_id):
-        val = self.r.hget("restarts", str(worker_id))
-        return int(val)
+        path = os.path.join(self.key_paths["restarts"], str(worker_id) + ".json")
+        with open(path, "r") as f:
+            count = int(json.load(f))
+        return count
 
     # current etl task
     def etl_set_task(self, task, mode):
-        res = self.r.set("etl_task", json.dumps([task, mode]))
-        return res
+        d = {"task": task, "mode": mode}
+        path = self.key_paths["etl_task"]
+        with open(path, "w+") as f:
+            json.dump(d, f)
 
     def etl_get_task(self):
-        task, mode = json.loads(self.r.get("etl_task"))
+        path = self.key_paths["etl_task"]
+        with open(path, "r") as f:
+            d = json.load(f)
+        task, mode = d["task"], d["mode"]
         return task, mode
 
     # save etl function strings
     def etl_set_spec(self, func):
         func_str = base64.b64encode(dill.dumps(func, byref=False)).decode("ascii")
-        res = self.r.set("etl_spec", func_str)
-        return res
+        path = self.key_paths["etl_spec"]
+        with open(path, "w+") as f:
+            json.dump(func_str, f)
 
     def etl_get_spec(self):
-        func_str = self.r.get("etl_spec")
+        path = self.key_paths["etl_spec"]
+        with open(path, "r") as f:
+            func_str = json.load(f)
         func = dill.loads(base64.b64decode(func_str))
         return func
 
     # current etl-worker status
     def etl_set_worker_status(self, worker_id, status):
-        res = self.r.hset("etl_worker_status", str(worker_id), status)
-        return res
+        path = os.path.join(self.key_paths["etl_worker_status"], str(worker_id) + ".json")
+        with open(path, "w+") as f:
+            json.dump(status, f)
 
     def etl_get_worker_status(self, worker_id):
-        val = self.r.hget("etl_worker_status", str(worker_id))
+        path = os.path.join(self.key_paths["etl_worker_status"], str(worker_id) + ".json")
+        with open(path, "r") as f:
+            val = json.load(f)
         return val
 
     # current etl-worker progress
     def etl_set_worker_progress(self, worker_id, progress):
-        res = self.r.hset("etl_worker_progress", str(worker_id), progress)
-        return res
+        path = os.path.join(self.key_paths["etl_worker_progress"], str(worker_id) + ".json")
+        with open(path, "w+") as f:
+            json.dump(progress, f)
 
     def etl_get_worker_progress(self, worker_id):
-        val = self.r.hget("etl_worker_progress", str(worker_id))
-        return float(val)
+        path = os.path.join(self.key_paths["etl_worker_progress"], str(worker_id) + ".json")
+        with open(path, "r") as f:
+            progress = float(json.load(f))
+        return progress
 
     # save mop function strings
     def mop_set_spec(self, func):
         func_str = base64.b64encode(dill.dumps(func, byref=False)).decode("ascii")
-        res = self.r.set("mop_spec", func_str)
-        return res
+        path = self.key_paths["mop_spec"]
+        with open(path, "w+") as f:
+            json.dump(func_str, f)
 
     def mop_get_spec(self):
-        func_str = self.r.get("mop_spec")
+        path = self.key_paths["mop_spec"]
+        with open(path, "r") as f:
+            func_str = json.load(f)
         func = dill.loads(base64.b64decode(func_str))
         return func
 
     # current mop task
     def mop_set_task(self, value):
-        res = self.r.set("mop_task", value)
-        return res
+        path = self.key_paths["mop_task"]
+        with open(path, "w+") as f:
+            json.dump(value, f)
 
     def mop_get_task(self):
-        tasks = self.r.get("mop_task")
-        return tasks
+        path = self.key_paths["mop_task"]
+        with open(path, "r") as f:
+            val = json.load(f)
+        return val
 
     # model for each mop-worker to run
     def mop_set_model_on_worker(self, worker_id, epoch, model_id, is_last_worker):
-        records = json.loads(self.r.hget("mop_model_on_worker", str(worker_id)))
+        path = os.path.join(self.key_paths["mop_model_on_worker"], str(worker_id) + ".json")
+        if os.path.isfile(path):
+            with open(path, "r") as f:
+                records = json.load(f)
+        else:
+            records = list()
         d = {
             "epoch": epoch,
             "model_id": model_id,
             "is_last_worker": is_last_worker,
         }
         records.append(d)
-        res = self.r.hset("mop_model_on_worker", str(worker_id), json.dumps(records))
-        return res
+
+        with open(path, "w+") as f:
+            json.dump(records, f)
 
     def mop_get_models_on_worker(self, worker_id, latest_only=True):
-        prev = json.loads(self.r.hget("mop_model_on_worker", str(worker_id)))
+        path = os.path.join(self.key_paths["mop_model_on_worker"], str(worker_id) + ".json")
+        with open(path, "r") as f:
+            prev = json.load(f)
         if latest_only:
             d = prev[-1]
             d["epoch"] = int(d["epoch"])
@@ -194,53 +258,82 @@ class KeyValueStore:
 
     # (model, parallelism) for each mop-worker to sample
     def mop_set_model_parallelism_on_worker(self, worker_id, mpl):
-        d = json.dumps(mpl)
-        res = self.r.hset("mop_model_parallelism_on_worker", str(worker_id), d)
-        return res
+        path = self.key_paths["mop_model_parallelism_on_worker"]
+        if os.path.isfile(path):
+            with open(path, "r") as f:
+                records = json.load(f)
+        else:
+            records = {}
+        records[str(worker_id)] = mpl
+        with open(path, "w+") as f:
+            json.dump(records, f)
 
     def mop_get_model_parallelism_on_worker(self, worker_id):
-        val = json.loads(self.r.hget("mop_model_parallelism_on_worker", str(worker_id)))
-        return val
+        path = self.key_paths["mop_model_parallelism_on_worker"]
+        with open(path, "r") as f:
+            records = json.load(f)
+        return records[str(worker_id)]
 
     # current mop-worker status
     def mop_set_worker_status(self, worker_id, status):
-        res = self.r.hset("mop_worker_status", str(worker_id), status)
-        return res
+        path = os.path.join(self.key_paths["mop_worker_status"], str(worker_id) + ".json")
+        with open(path, "w+") as f:
+            json.dump(status, f)
 
     def mop_get_worker_status(self, worker_id):
-        val = self.r.hget("mop_worker_status", str(worker_id))
-        return val
+        path = os.path.join(self.key_paths["mop_worker_status"], str(worker_id) + ".json")
+        with open(path, "r") as f:
+            status = json.load(f)
+        return status
 
     # model_id to model mapping
     def mop_set_model_mapping(self, model_map):
-        res = self.r.set("mop_model_mapping", json.dumps(model_map))
-        return res
+        path = self.key_paths["mop_model_mapping"]
+        with open(path, "w+") as f:
+            json.dump(model_map, f)
 
     def mop_get_model_mapping(self, model_id=None):
-        d = json.loads(self.r.get("mop_model_mapping"))
+        path = self.key_paths["mop_model_mapping"]
+        with open(path, "r") as f:
+            model_map = json.load(f)
         if model_id is not None:
-            val = d[str(model_id)]
+            val = model_map[str(model_id)]
             return val
         else:
-            val = {int(key): value for key, value in d.items()}
+            val = {int(key): value for key, value in model_map.items()}
             return val
 
     # model_id to parallelism mapping
     def mop_set_parallelism_mapping(self, model_id, parallelism):
-        res = self.r.hset("mop_parallelism_mapping", str(model_id), parallelism)
-        return res
+        path = self.key_paths["mop_parallelism_mapping"]
+        if os.path.isfile(path):
+            with open(path, "r") as f:
+                records = json.load(f)
+        else:
+            records = {}
+        records[str(model_id)] = parallelism
+        with open(path, "w+") as f:
+            json.dump(records, f)
 
     def mop_get_parallelism_mapping(self, model_id):
-        val = self.r.hget("mop_parallelism_mapping", str(model_id))
+        path = self.key_paths["mop_parallelism_mapping"]
+        with open(path, "r") as f:
+            records = json.load(f)
+            val = records[str(model_id)]
         return val
 
     # temp save sample time
     def mop_set_sample_time(self, model_id, parallelism, time_taken):
+        path = os.path.join(self.key_paths["mop_sample_time"], str(model_id) + ".json")
         mp = json.dumps((model_id, parallelism))
-        res = self.r.hset("mop_sample_time", mp, str(time_taken))
-        return res
+        d = {mp: time_taken}
+        with open(path, "w+") as f:
+            json.dump(d, f)
 
     def mop_get_sample_time(self, model_id, parallelism):
+        path = os.path.join(self.key_paths["mop_sample_time"], str(model_id) + ".json")
         mp = json.dumps((model_id, parallelism))
-        val = self.r.hget("mop_sample_time", mp)
-        return float(val)
+        with open(path, "r") as f:
+            d = json.load(f)
+            val = float(d[mp])
+        return val
