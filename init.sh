5ff5212678ff89b731db6ecbfd2e108ceb5a1445
prsridha <prsridha@ucsd.edu>
2023-12-07 00:04:15 -0800

    fixed minor issue in init.sh

--- init.sh
+++ init.sh
@@ -9,6 +9,6 @@ cp /cerebro-core/cerebro/misc/experiment.ipynb /user/experiment.ipynb
 # start notebook in user dir
 JUPYTER_TOKEN=$(echo "projectcerebro" | xxd -u -l 14 -p)
 jupyter notebook --generate-config
-sed -i "448s/.*/c.NotebookApp.notebook_dir = '\/user'/" /root/.jupyter/jupyter_notebook_config.py
+sed -i "448s/.*/c.NotebookApp.notebook_dir = '\/user'/" $HOME/.jupyter/jupyter_notebook_config.py
 jupyter nbextension enable --py widgetsnbextension
 jupyter notebook --NotebookApp.token=$JUPYTER_TOKEN --NotebookApp.password=$JUPYTER_TOKEN --ip 0.0.0.0 --allow-root --no-browser
