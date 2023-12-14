5cb286f4898be3ec08a0956a76dade463a0fec72
prsridha <prsridha@ucsd.edu>
2023-12-13 16:45:10 -0800

    fixed bug in voyager file IO - src_file path issue

--- cerebro/util/voyager_io.py
+++ cerebro/util/voyager_io.py
@@ -27,29 +27,41 @@ class VoyagerIO:
         completed_size = 0
         self.set_total_size(local_prefix)
 
-        for root, dirs, files in os.walk(local_prefix):
-            for file in files:
-                full_src_path = os.path.join(root, file)
-                completed_size += os.path.getsize(full_src_path)
-                remaining_prefix = full_src_path.replace(exclude_prefix, "").lstrip(os.path.sep)
-                dest_path = os.path.join(remote_prefix, remaining_prefix) if remaining_prefix else remote_prefix
-                Path(os.path.dirname(dest_path)).mkdir(parents=True, exist_ok=True)
-                shutil.copy(full_src_path, dest_path)
-                self.update_progress(completed_size)
+        all_files = []
+        if os.path.isfile(local_prefix):
+            all_files.append(local_prefix)
+        else:
+            for root, dirs, files in os.walk(local_prefix):
+                for file in files:
+                    all_files.append(os.path.join(root, file))
+
+        for full_src_path in all_files:
+            completed_size += os.path.getsize(full_src_path)
+            remaining_prefix = full_src_path.replace(exclude_prefix, "").lstrip(os.path.sep)
+            dest_path = os.path.join(remote_prefix, remaining_prefix) if remaining_prefix else remote_prefix
+            Path(os.path.dirname(dest_path)).mkdir(parents=True, exist_ok=True)
+            shutil.copy(full_src_path, dest_path)
+            self.update_progress(completed_size)
 
     def download(self, local_prefix, remote_prefix, exclude_prefix=""):
         completed_size = 0
         self.set_total_size(remote_prefix)
 
-        for root, dirs, files in os.walk(remote_prefix):
-            for file in files:
-                full_src_path = os.path.join(root, file)
-                completed_size += os.path.getsize(full_src_path)
-                remaining_prefix = full_src_path.replace(exclude_prefix, "").lstrip(os.path.sep)
-                dest_path = os.path.join(local_prefix, remaining_prefix) if remaining_prefix else local_prefix
-                Path(os.path.dirname(dest_path)).mkdir(parents=True, exist_ok=True)
-                shutil.copy(full_src_path, dest_path)
-                self.update_progress(completed_size)
+        all_files = []
+        if os.path.isfile(remote_prefix):
+            all_files.append(remote_prefix)
+        else:
+            for root, dirs, files in os.walk(remote_prefix):
+                for file in files:
+                    all_files.append(os.path.join(root, file))
+
+        for full_src_path in all_files:
+            completed_size += os.path.getsize(full_src_path)
+            remaining_prefix = full_src_path.replace(exclude_prefix, "").lstrip(os.path.sep)
+            dest_path = os.path.join(local_prefix, remaining_prefix) if remaining_prefix else local_prefix
+            Path(os.path.dirname(dest_path)).mkdir(parents=True, exist_ok=True)
+            shutil.copy(full_src_path, dest_path)
+            self.update_progress(completed_size)
 
     def list_files(self, path):
         files = []
