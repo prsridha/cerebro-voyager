9a54a173c6ebf13f5af076899a0d8b47ac28958b
prsridha <prsridha@ucsd.edu>
2024-01-20 14:05:01 -0800

    debugging mem issue in ddp

--- cerebro/parallelisms/ddp.py
+++ cerebro/parallelisms/ddp.py
@@ -205,10 +205,10 @@ class DDPExecutor(Parallelism):
         gc.collect()
         clean_up()
 
-    def _execute_hmm(self, rank, dataset):
-        print("Yaayyy")
+    def _execute_hmm(self, rank, user_train_func_str, user_metrics_func_str):
+        print("Yaayyy", user_train_func_str)
         setup(rank, self.world_size)
-        print("done")
+        print("done", user_metrics_func_str)
 
     def execute_sample(self, minibatch_spec, dataset):
         # set values
@@ -228,7 +228,7 @@ class DDPExecutor(Parallelism):
 
         # spawn DDP workers
         # mp.spawn(self._execute_inner, args=(dataset, user_train_func_str, user_metrics_func_str), nprocs=self.world_size, join=True)
-        mp.spawn(self._execute_hmm, args=(dataset,), nprocs=self.world_size, join=True)
+        mp.spawn(self._execute_hmm, args=(user_train_func_str, user_metrics_func_str), nprocs=self.world_size, join=True)
 
     def execute_val(self, minibatch_spec, dataset, model_id):
         # get val and metrics_agg functions
