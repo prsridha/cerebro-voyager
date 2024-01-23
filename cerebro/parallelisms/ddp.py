1494eb963b90f104e38f4034602ab2082034cc2e
prsridha <prsridha@ucsd.edu>
2024-01-22 17:00:31 -0800

    debugging error in ddp

--- cerebro/parallelisms/ddp.py
+++ cerebro/parallelisms/ddp.py
@@ -237,8 +237,10 @@ class DDPExecutor(Parallelism):
         dataloader = DataLoader(dataset, batch_size=self.hyperparams["batch_size"], sampler=sampler, shuffle=False)
 
         # load models and their state dicts
-        # model_object = torch.load("/data/checkpoint_storage/model_1/model_object_1.pt")
-        model_object = torch.load(self.model_path)
+        print("IT IS  - ", self.model_path)
+        print("Needs to be - /data/checkpoint_storage/model_10/model_object_10.pt")
+        model_object = torch.load("/data/checkpoint_storage/model_10/model_object_10.pt")
+        # model_object = torch.load(self.model_path)
         updated_obj = self.load_checkpoint(model_object)
 
         # parallelize models from model object file
