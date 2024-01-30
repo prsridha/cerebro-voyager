6a8108ae40cc7f3a9941b60236304214a08b1dcb
prsridha <prsridha@ucsd.edu>
2024-01-29 16:04:24 -0800

    fixed test progress bar

--- cerebro/mop/mop_controller.py
+++ cerebro/mop/mop_controller.py
@@ -400,7 +400,7 @@ class MOPController:
             self.kvs.mop_set_task(kvs_constants.MOP_TASK_TEST, worker_id, task_id)
 
         # update completion progress
-        progress = tqdm_notebook(total=100, desc="Testing Progress", position=0, leave=False)
+        progress = tqdm_notebook(total=self.num_workers, desc="Testing Progress", position=0, leave=False)
         while True:
             all_complete = []
             for w in range(self.num_workers):
