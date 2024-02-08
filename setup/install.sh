48af48ae5b855ec3acceceb2ed8de7ca8d7969ab
prsridha <prsridha@ucsd.edu>
2024-02-07 23:16:15 -0800

    added cerebro setup files

---
+++ setup/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# for OpenSSL issue with Kubernetes
+pip install pip --upgrade
+pip install pyopenssl --upgrade
+
+# add Cerebro installer's dependencies
+pip install -r requirements.txt --user
+
+echo "Cerebro dependencies installed successfully\n"
